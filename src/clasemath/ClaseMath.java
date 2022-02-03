d5929229f86530ffbb51205dd7a4c14b6a70a9a9
David G <david@ejemplo.com>
2022-02-03 16:57:48 +0100

    Primer commit com1

---
+++ src/clasemath/ClaseMath.java
@@ -0,0 +1,15 @@
+package clasemath;
+
+public class ClaseMath {
+
+	public static void main(String[] args) {
+
+		//2 elevado a 3
+		double x = Math.pow(2, 3);
+		System.out.println(x);
+		
+		//raiz cuadrada de 3
+		System.out.println("La raiz cuadrada de 3 es: " + (Math.sqrt(3)));
+	}
+
+}
